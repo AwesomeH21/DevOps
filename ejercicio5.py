042fbabc84a57bc1c4c90e815e22552b66fd9f9c
AwesomeH21 <2023371025@uteq.edu.mx>
2024-09-06 09:47:57 -0600

    trabajo clase
    ejercicioclase

--- ejercicio5.py
+++ ejercicio5.py
@@ -1,6 +1,6 @@
 for i in range(1,11):
     modd=i%2
     if modd == 0:
-        print("número par")
+        print(i,"número par")
     else :
-        print("número impar")
+        print(i,"número impar")
---
+++ ejerciciowhile1.py
@@ -0,0 +1,8 @@
+num=int(0)
+while num >= 0:
+    num=int(input("Dame un numero negativo "))
+    if num < 0:
+        print("Danke sehr")
+        num=-1
+    else:
+        print("Dame un fokin número negativo ")
