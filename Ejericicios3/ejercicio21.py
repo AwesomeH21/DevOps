d6e296fdb44e3d52aaf72d5bff861935b0be2685
AwesomeH21 <2023371025@uteq.edu.mx>
2024-09-21 17:06:25 -0600

    Pequeño update
    update al ejercicio 21

--- Ejericicios3/ejercicio21.py
+++ Ejericicios3/ejercicio21.py
@@ -1,15 +1,21 @@
 vent=list()
+y=int(1)
+sum=int(0)
 for i in range(0,5,1):
-    num=int(input("Ingresa el número de ventas"))
+    num=int(input("Ingresa el número de ventas "))
     if (num >= 0):
         vent.append(num)
-print(vent)
+
 for x in range(0,5,1):
     if (vent[x] > 500):
-        print(f"la categoría {x} tiene Ventas altas")
+        print(f"la categoría {y} tiene Ventas altas")
     elif (vent[x] >= 100 and vent[x] <= 500):
-        print(f"la categoría {x} tiene Ventas moderadas")
+        print(f"la categoría {y} tiene Ventas moderadas")
     elif (vent[x] > 0 and vent[x] < 100):
-        print(f"la categoría {x} tiene Ventas bajas")
+        print(f"la categoría {y} tiene Ventas bajas")
     else:
         print("Se ingresó una cantidad incorrecta para esta cateogría")
+    y=y+1
+for z in range(0,5,1):
+    sum=sum+vent[z]
+print(f"El total de ventas fue de {sum}")
